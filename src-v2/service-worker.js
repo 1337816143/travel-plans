024881d0adc635ad0662beb8b0bb5cf49c2c993c
1337816143 <1337816143@qq.com>
2026-07-27 16:51:47 +0800

    Version service worker assets with the active v2 release

--- src-v2/service-worker.js
+++ src-v2/service-worker.js
@@ -2,11 +2,11 @@ const CACHE='travel-plans-__VERSION__';
 const CORE=[
   './',
   './index.html',
-  './versions/2026-07-27-v2.0.0.html',
-  './assets/v2.0.0/payload-0.b64',
-  './assets/v2.0.0/payload-1.b64',
-  './assets/v2.0.0/payload-2.b64',
-  './assets/v2.0.0/payload-3.b64',
+  './versions/2026-07-27-v__VERSION__.html',
+  './assets/v__VERSION__/payload-0.b64',
+  './assets/v__VERSION__/payload-1.b64',
+  './assets/v__VERSION__/payload-2.b64',
+  './assets/v__VERSION__/payload-3.b64',
   './versions/2026-07-27-v1.0.15.html'
 ];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
